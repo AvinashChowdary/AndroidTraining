0ed5ae7ef86ab220dec62d6fca19a5b047ae0174
Avinash <avinash.ravilla@paradigmcreatives.com>
2014-09-09 13:01:34 +0530

    Git Ignore File

--- Parcel/gen/com/pcs/parcel/R.java
+++ Parcel/gen/com/pcs/parcel/R.java
@@ -10,11 +10,38 @@ package com.pcs.parcel;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int banner=0x7f040000;
+        public static final int bg=0x7f040004;
+        public static final int button=0x7f040003;
+        public static final int label=0x7f040001;
+        public static final int msg=0x7f040005;
+        public static final int txt=0x7f040002;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int data=0x7f070000;
+        public static final int dept_edt=0x7f070003;
+        public static final int empid_edt=0x7f070002;
+        public static final int name_edt=0x7f070001;
+        public static final int salary_edt=0x7f070004;
+        public static final int submit_btn=0x7f070005;
+    }
+    public static final class layout {
+        public static final int receiving=0x7f030000;
+        public static final int sending=0x7f030001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
+        public static final int button_txt=0x7f050007;
+        public static final int dept_hint=0x7f050003;
+        public static final int empid_hint=0x7f050002;
+        public static final int label_rec=0x7f050006;
+        public static final int label_send=0x7f050005;
+        public static final int name_hint=0x7f050001;
+        public static final int salary_hint=0x7f050004;
     }
     public static final class style {
         /** 
@@ -38,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
