23b4a09ef534adc23fced43ba6234f5938cad518
Avinash <avinash.ravilla@paradigmcreatives.com>
2014-09-10 12:23:15 +0530

    Layout Updated

--- ImplicitIntent/gen/com/pcs/implicitintent/R.java
+++ ImplicitIntent/gen/com/pcs/implicitintent/R.java
@@ -13,8 +13,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int call=0x7f060000;
+    }
+    public static final class layout {
+        public static final int layout=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int browser=0x7f040004;
+        public static final int call_btn=0x7f040001;
+        public static final int contacts_btn=0x7f040003;
+        public static final int mail_btn=0x7f040002;
     }
     public static final class style {
         /** 
@@ -38,10 +48,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
