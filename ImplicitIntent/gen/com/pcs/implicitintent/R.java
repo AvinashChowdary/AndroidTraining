82637f52bffb7d1df8d11fc02a0dd36a2fe12fb4
Avinash <avinash.ravilla@paradigmcreatives.com>
2014-09-10 14:52:41 +0530

    Completed Implicit Intent

--- ImplicitIntent/gen/com/pcs/implicitintent/R.java
+++ ImplicitIntent/gen/com/pcs/implicitintent/R.java
@@ -10,21 +10,32 @@ package com.pcs.implicitintent;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int banner=0x7f040000;
+        public static final int white=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int contacts=0x7f020000;
+        public static final int dailer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int mail=0x7f020003;
+        public static final int music=0x7f020004;
+        public static final int safari=0x7f020005;
     }
     public static final class id {
-        public static final int call=0x7f060000;
+        public static final int browser=0x7f070001;
+        public static final int call=0x7f070000;
+        public static final int contacts=0x7f070002;
+        public static final int mail=0x7f070003;
+        public static final int music=0x7f070004;
     }
     public static final class layout {
         public static final int layout=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int browser=0x7f040004;
-        public static final int call_btn=0x7f040001;
-        public static final int contacts_btn=0x7f040003;
-        public static final int mail_btn=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int banner_txt=0x7f050002;
+        public static final int chooser=0x7f050001;
     }
     public static final class style {
         /** 
@@ -48,10 +59,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
