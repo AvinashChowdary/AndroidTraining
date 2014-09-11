801272ef86bae03e047f179573134e256b9b2939
AvinashChowdary <avinash.ravilla@paradigmcreatives.com>
2014-09-12 01:01:22 +0530

    completed

---
+++ Sender/src/com/pcs/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.pcs.constants;
+
+
+
+public class Constants {
+	public static interface InentExtras{
+		public static final String TEXT = "com.pcs.receiver.SHOW_TEXT";
+		public static final String IMAGE = "com.pcs.receiver.SHOW_IMAGE";
+		public static final String WEB = "com.pcs.receiver.SHOW_WEB";
+		 
+	}
+
+}
