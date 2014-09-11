51bd0481e84da264672f23ec92e15e4a10b65f7e
Avinash Ravilla <avinash.ravilla@paradigmcreatives.com>
2014-09-11 12:25:31 +0530

    src , constants added , initial work done

--- CallMeApp/Sender/gen/com/pcs/sender/R.java
+++ CallMeApp/Sender/gen/com/pcs/sender/R.java
@@ -30,6 +30,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int banner=0x7f050004;
         public static final int image_txt=0x7f050001;
         public static final int text_txt=0x7f050002;
         public static final int webpage_txt=0x7f050003;
