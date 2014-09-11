e38ef33c8c36a314e321db41c63611e4fe32ebc7
Avinash Ravilla <avinash.ravilla@paradigmcreatives.com>
2014-09-11 12:01:10 +0530

    Sender package Manifest and Layout updated

--- CallMeApp/Sender/gen/com/pcs/sender/R.java
+++ CallMeApp/Sender/gen/com/pcs/sender/R.java
@@ -10,11 +10,29 @@ package com.pcs.sender;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040003;
+        public static final int pink=0x7f040001;
+        public static final int purple=0x7f040000;
+        public static final int white=0x7f040002;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int image=0x7f070000;
+        public static final int text=0x7f070001;
+        public static final int webpage=0x7f070002;
+    }
+    public static final class layout {
+        public static final int sending=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
+        public static final int image_txt=0x7f050001;
+        public static final int text_txt=0x7f050002;
+        public static final int webpage_txt=0x7f050003;
     }
     public static final class style {
         /** 
@@ -38,10 +56,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
