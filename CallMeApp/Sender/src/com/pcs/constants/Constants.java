b42c0276aba395336da9ea6dc918b8e994fc2978
Avinash Ravilla <avinash.ravilla@paradigmcreatives.com>
2014-09-11 12:26:49 +0530

    src , constants added , initial work done

---
+++ CallMeApp/Sender/src/com/pcs/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.pcs.constants;
+
+public class Constants {
+	public static interface IntentExtras {
+		
+		public static final String IMAGE = "com.pcs.opener.OpeningActivity.SHOW_IMAGE";
+		public static final String TEXT = "com.pcs.opener.OpeningActivity.SHOW_TEXT";
+		public static final String WEBPAGE = "com.pcs.opener.OpeningActivity.SHOW_WEBPAGE";
+	}
+
+}
