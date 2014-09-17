de0469ec8ab61a453d0eba4950640b1471657b66
Avinash <avinash.ravilla@paradigmcreatives.com>
2014-09-17 12:01:50 +0530

    Started Dialog Demo and created layout

---
+++ Contacts/src/com/pcs/contacts/DialogActivity.java
@@ -0,0 +1,14 @@
+package com.pcs.contacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
