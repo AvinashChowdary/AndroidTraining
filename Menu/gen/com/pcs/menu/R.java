e65b20f2a63299117e86d76ca803df285f882a4b
Sailika <sailika.ethalapaka@paradigmcreatives.com>
2014-09-19 16:53:38 +0530

    Options and Context menu created

--- Menu/gen/com/pcs/menu/R.java
+++ Menu/gen/com/pcs/menu/R.java
@@ -10,38 +10,67 @@ package com.pcs.menu;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040001;
+        public static final int cyan=0x7f040004;
+        public static final int grey=0x7f040002;
+        public static final int lightblue=0x7f040003;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040005;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int share_icon=0x7f020001;
+        public static final int image1=0x7f020001;
+        public static final int image2=0x7f020002;
+        public static final int image3=0x7f020003;
+        public static final int image4=0x7f020004;
+        public static final int image5=0x7f020005;
+        public static final int image6=0x7f020006;
+        public static final int share_icon=0x7f020007;
     }
     public static final class id {
-        public static final int about=0x7f060008;
-        public static final int contatc=0x7f060009;
-        public static final int delete=0x7f060002;
-        public static final int details=0x7f060004;
-        public static final int edit=0x7f060001;
-        public static final int rate=0x7f060007;
-        public static final int setas=0x7f060003;
-        public static final int settings=0x7f060006;
-        public static final int share=0x7f060005;
-        public static final int view=0x7f060000;
+        public static final int about=0x7f08000e;
+        public static final int contatc=0x7f08000f;
+        public static final int delete=0x7f080009;
+        public static final int details=0x7f08000b;
+        public static final int edit=0x7f080008;
+        public static final int image1=0x7f080001;
+        public static final int image2=0x7f080002;
+        public static final int image3=0x7f080004;
+        public static final int image4=0x7f080003;
+        public static final int image5=0x7f080005;
+        public static final int image6=0x7f080006;
+        public static final int rate=0x7f08000d;
+        public static final int setas=0x7f08000a;
+        public static final int settings=0x7f08000c;
+        public static final int share=0x7f080000;
+        public static final int view=0x7f080007;
+    }
+    public static final class layout {
+        public static final int gallery=0x7f030000;
     }
     public static final class menu {
-        public static final int context_menu=0x7f050000;
-        public static final int options_menu=0x7f050001;
+        public static final int context_menu=0x7f070000;
+        public static final int options_menu=0x7f070001;
     }
     public static final class string {
-        public static final int about_us=0x7f030004;
-        public static final int app_name=0x7f030000;
-        public static final int contact_us=0x7f030003;
-        public static final int delete_txt=0x7f030007;
-        public static final int details_txt=0x7f030009;
-        public static final int edit_txt=0x7f030006;
-        public static final int rate_txt=0x7f030002;
-        public static final int set_as_txt=0x7f030008;
-        public static final int settings_txt=0x7f030001;
-        public static final int share_txt=0x7f03000a;
-        public static final int view_txt=0x7f030005;
+        public static final int about_us=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int banner_txt=0x7f05000b;
+        public static final int contact_us=0x7f050003;
+        public static final int delete_toast=0x7f05000e;
+        public static final int delete_txt=0x7f050007;
+        public static final int details_toast=0x7f050010;
+        public static final int details_txt=0x7f050009;
+        public static final int edit_toast=0x7f05000d;
+        public static final int edit_txt=0x7f050006;
+        public static final int rate_txt=0x7f050002;
+        public static final int set_as_txt=0x7f050008;
+        public static final int setas_toast=0x7f05000f;
+        public static final int settings_txt=0x7f050001;
+        public static final int share_txt=0x7f05000a;
+        public static final int view_toast=0x7f05000c;
+        public static final int view_txt=0x7f050005;
     }
     public static final class style {
         /** 
@@ -65,10 +94,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
