b60ed0d97518fbfe4b9064a3d4e4e70643d8212d
Avinash <avinash.ravilla@paradigmcreatives.com>
2014-09-23 16:26:53 +0530

    Progress Dialog fixed

--- Dowloader/src/com/pcs/dowloader/MainActivity.java
+++ Dowloader/src/com/pcs/dowloader/MainActivity.java
@@ -1,89 +1,191 @@
 package com.pcs.dowloader;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+public class MainActivity extends Activity {
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private static TextView idTxt;
+	private static TextView aboutTxt;
+	private static TextView categoryTxt;
+	private static TextView checkinsTxt;
+	private static ProgressDialog  progressDialog ;
+	private static final int DIALOG = 9;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		idTxt = (TextView) findViewById(R.id.id_display);
+		aboutTxt = (TextView) findViewById(R.id.about_display);
+		checkinsTxt = (TextView) findViewById(R.id.checkins_display);
+		categoryTxt = (TextView) findViewById(R.id.category_display);
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+		Button downloadBtn = (Button) findViewById(R.id.download_btn);
+		downloadBtn.setOnClickListener(new OnClickListener() {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-    
-    public static class Downloader extends AsyncTask<Void, Void, Void>{
+			@Override
+			public void onClick(View v) {
+				new Downloader().execute();
+			}
+		});
+
+	}
+
+
+	public Dialog onCreateDialog(int id){
+		
+		switch (id) {
+		case DIALOG:
+			progressDialog = new ProgressDialog(this);
+			progressDialog.setTitle(R.string.title);
+			progressDialog.setProgress(1);
+			progressDialog.setMax(100);
+			progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			progressDialog.setCancelable(true);
+			progressDialog.show();
+			return progressDialog;
+		
+		default:
+			return null;
+		}
+		
+	}
+	
+	public class Downloader extends AsyncTask<Void, Integer, String>{
+
+		private Context mContext;
+		private StringBuilder stringBuilder=null;
+		
+
+		public Downloader(Context context){
+			mContext = context;
+		}
+
+		public Downloader() {
+		}
 
 		@Override
 		protected void onPreExecute() {
+
 			super.onPreExecute();
+			showDialog(DIALOG);
 		}
-    	
-    	@Override
-		protected Void doInBackground(Void... params) {
-			return null;
+
+
+		@Override
+		protected String doInBackground(Void... params) {
+
+			String line=null;
+
+			try {
+
+				URL url = new URL("https://graph.facebook.com/19292868552");
+				URLConnection connection= url.openConnection();
+
+				connection.connect();
+
+				InputStream inputStream = connection.getInputStream();
+				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+				BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+
+				stringBuilder = new StringBuilder();
+
+				while ((line = bufferReader.readLine()) != null) {
+
+					
+					publishProgress(70);
+					stringBuilder.append(line);
+
+				}
+
+
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return stringBuilder.toString();
 		}
-    	
-    	@Override
-    	protected void onProgressUpdate(Void... values) {
-    		super.onProgressUpdate(values);
-    	}
-    	
-    	@Override
-    	protected void onPostExecute(Void result) {
-    		super.onPostExecute(result);
-    	}
-    	
-    }
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			progressDialog.setProgress(values[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+						
+			super.onPostExecute(result);
+			dismissDialog(DIALOG);
+
+			try {
+
+				JSONObject jsonObj = new JSONObject(result);
+
+				if(jsonObj.has("id")){
+
+					String id = jsonObj.getString("id");
+					idTxt.setText("ID : "+id+"\n");
+				}
+				if(jsonObj.has("about")){
+
+					String about = jsonObj.getString("about");
+					aboutTxt.setText("About : "+about+"\n");
+				}
+				if(jsonObj.has("category")){
+
+					String category = jsonObj.getString("category");
+					categoryTxt.setText("Category : "+category+"\n");
+				}
+				if(jsonObj.has("checkins")){
+
+					String checkins = jsonObj.getString("checkins");
+					checkinsTxt.setText("Checkins : "+checkins+"\n");
+				}
+
+				
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+
+
+	}
+
 }
+
