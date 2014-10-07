e0725b142a83c961a1aa82933f66e8ee42235d51
AvinashChowdary <avinash.ravilla@paradigmcreatives.com>
2014-10-08 01:20:32 +0530

    Insertion and display completed

---
+++ ContactsDB/src/com/pcs/adapters/ContactsAdapter.java
@@ -0,0 +1,58 @@
+package com.pcs.adapters;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.widget.SimpleCursorAdapter;
+
+import com.example.contactsdb.R;
+import com.example.helpers.MySQLiteHelper;
+import com.example.helpers.MySQLiteHelper.ContactsContrast;
+import com.example.model.Contact;
+
+public class ContactsAdapter {
+
+	private SQLiteDatabase mDataBase;
+	private SimpleCursorAdapter cursorAdapter;
+	private MySQLiteHelper mHelper;
+	private Context mContext;
+	
+	public ContactsAdapter(Context context){
+		mContext = context;
+		mHelper = new MySQLiteHelper(mContext);
+	}
+	
+	public void addContact(Contact contact) throws SQLException {
+
+		mDataBase = mHelper.getWritableDatabase();
+
+		ContentValues mValues = new ContentValues();
+		mValues.put(ContactsContrast.CONTACT_NAME, contact.getName());
+		mValues.put(ContactsContrast.CONTACT_EMAIL, contact.getEmail());
+		mValues.put(ContactsContrast.CONTACT_NUMBER, contact.getNumber());
+
+		mDataBase.insert(ContactsContrast.TABLE_NAME, null, mValues);
+		
+
+	}
+
+	public SimpleCursorAdapter showContacts() {
+
+		Cursor mCursor = mDataBase.query(ContactsContrast.TABLE_NAME, null,
+				null, null, null, null, null);
+
+		if (mCursor != null) {
+			cursorAdapter = new SimpleCursorAdapter(mContext,
+					R.layout.listview, mCursor, new String[] { "_id", "_name",
+							"_number", "_email" }, new int[] { R.id.display_id,
+							R.id.display_name, R.id.display_number,
+							R.id.display_email });
+
+		}
+		
+		return cursorAdapter;
+	}
+
+}
