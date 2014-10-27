157d84ad062aeb7952e6dc97e4c4ccbd307717d8
AvinashChowdary <avinash.ravilla@paradigmcreatives.com>
2014-10-27 21:12:23 +0530

    Completed moving other views, have to make image full screen animation

---
+++ ExpandingAnimation/src/com/example/expandinganimation/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.expandinganimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private ImageView red, green, blue;
+	private Animation zoom, slideUp, slideDown;
+	private static final int RED = 1, GREEN = 2, BLUE = 3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		red = (ImageView) findViewById(R.id.red);
+		green = (ImageView) findViewById(R.id.green);
+		blue = (ImageView) findViewById(R.id.blue);
+
+		red.setOnClickListener(this);
+		green.setOnClickListener(this);
+		blue.setOnClickListener(this);
+
+		zoom = AnimationUtils.loadAnimation(this, R.anim.animation);
+		slideUp = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+		slideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.red:
+			clicked(RED);
+			break;
+		case R.id.green:
+			clicked(GREEN);
+			break;
+		case R.id.blue:
+			clicked(BLUE);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void clicked(int color) {
+
+		LinearLayout.LayoutParams layoutParams = new LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+
+		if (color == RED) {
+			red.startAnimation(zoom);
+			blue.startAnimation(slideDown);
+			green.startAnimation(slideDown);
+		} else if (color == GREEN) {
+			green.startAnimation(zoom);
+			blue.startAnimation(slideDown);
+			red.startAnimation(slideUp);
+		} else {
+			blue.startAnimation(zoom);
+			red.startAnimation(slideUp);
+			green.startAnimation(slideUp);
+		}
+	}
+
+}
