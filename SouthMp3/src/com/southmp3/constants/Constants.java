11a9579011acb2c33dae056b4597b4bf7499a221
AvinashChowdary <avinash.ravilla@paradigmcreatives.com>
2015-03-20 18:53:50 +0530

    initial commit

---
+++ SouthMp3/src/com/southmp3/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.southmp3.constants;
+
+public class Constants {
+	public static final int HOME = 0;
+	public static final int TELUGU_SONGS = 1;
+	public static final int SEARCH = 2;
+	public static final int SHARE = 3;
+	public static final int RATE = 4;
+	public static final int CONTACT = 5;
+}
---
+++ SouthMp3/src/com/southmp3/fragment/ContactFragment.java
@@ -0,0 +1,7 @@
+package com.southmp3.fragment;
+
+import android.app.Fragment;
+
+public class ContactFragment extends Fragment {
+
+}
